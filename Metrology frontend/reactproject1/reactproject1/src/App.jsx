c616899f3198b593ba7c98ac535fe0be736bd651
Sait <said-bah@yandex.ru>
2026-04-16 23:51:14 +0300

    1st

---
+++ Metrology frontend/reactproject1/reactproject1/src/App.jsx
@@ -0,0 +1,11 @@
+import Btn from './buttons/buttons.jsx'
+
+function App() {
+    return (
+      <div>
+        <Btn/>
+        </div>
+  );
+}
+
+export default App;
---
+++ Metrology frontend/reactproject1/reactproject1/src/buttons/buttons.jsx
@@ -0,0 +1,9 @@
+import bs from './buttons.module.css';
+
+function Btn() {
+  return (
+      <button className={bs.blueButton}>Hello world!</button>
+  );
+}
+
+export default Btn;
